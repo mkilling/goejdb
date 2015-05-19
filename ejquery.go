ebeb766c0bf539f51b036cabe3d2cf99ad8c0be6
Ansgar Kellner <keans@gmx.de>
2015-05-19 19:43:12 +0200

    Closes #4 by using corresponding unsafe.Pointer calls and casts. Also adapted naming of library from tcejdb to ejdb

--- ejquery.go
+++ ejquery.go
@@ -1,23 +1,23 @@
 package goejdb
 
-// #cgo LDFLAGS: -ltcejdb
-// #include <tcejdb/ejdb.h>
+// #cgo LDFLAGS: -lejdb
+// #include <ejdb/ejdb.h>
 import "C"
 
 import "unsafe"
 
 // Query search mode flags in ejdbqryexecute()
 const (
-	// Query only count(*)
-	jbqrycount   = C.JBQRYCOUNT
-	// Fetch first record only
-	jbqryfindone = C.JBQRYFINDONE
+    // Query only count(*)
+    jbqrycount   = C.JBQRYCOUNT
+    // Fetch first record only
+    jbqryfindone = C.JBQRYFINDONE
 )
 
 // An EJDB query
 type EjQuery struct {
-	ptr  *[0]byte
-	ejdb *Ejdb
+    ptr  *[0]byte
+    ejdb *Ejdb
 }
 
 // Create query object.
@@ -88,36 +88,36 @@ type EjQuery struct {
 //
 //  NOTE: Only one index can be used in search query operation.
 func (ejdb *Ejdb) CreateQuery(query string, queries ...string) (*EjQuery, *EjdbError) {
-	query_bson := bson_from_json(query)
-	defer C.bson_destroy(query_bson)
+    query_bson := bson_from_json(query)
+    defer C.bson_destroy(query_bson)
 
-	ptr := C.ejdbcreatequery(ejdb.ptr, query_bson, nil, 0, nil)
-	if ptr == nil {
-		return nil, ejdb.check_error()
-	}
+    ptr := C.ejdbcreatequery((*C.struct_EJDB)(unsafe.Pointer(ejdb.ptr)), query_bson, nil, 0, nil)
+    if ptr == nil {
+        return nil, ejdb.check_error()
+    }
 
-	q := &EjQuery{ptr, ejdb}
+    q := &EjQuery{(*[0]byte)(unsafe.Pointer(ptr)), ejdb}
 
-	for _, orquery := range queries {
-		err := q.AddOr(orquery)
-		if err != nil {
-			q.Del()
-			return nil, ejdb.check_error()
-		}
-	}
+    for _, orquery := range queries {
+        err := q.AddOr(orquery)
+        if err != nil {
+            q.Del()
+            return nil, ejdb.check_error()
+        }
+    }
 
-	return q, nil
+    return q, nil
 }
 
 // Add OR restriction to query object. Expects orquery to be a JSON string.
 func (q *EjQuery) AddOr(orquery string) (*EjdbError) {
-	bson := bson_from_json(orquery)
-	defer C.bson_destroy(bson)
-	ret := C.ejdbqueryaddor(q.ejdb.ptr, q.ptr, unsafe.Pointer(bson.data))
-	if ret == nil {
-		return q.ejdb.check_error()
-	}
-	return nil
+    bson := bson_from_json(orquery)
+    defer C.bson_destroy(bson)
+    ret := C.ejdbqueryaddor((*C.struct_EJDB)(unsafe.Pointer(q.ejdb.ptr)), (*C.struct_EJQ)(unsafe.Pointer(q.ptr)), unsafe.Pointer(bson.data))
+    if ret == nil {
+        return q.ejdb.check_error()
+    }
+    return nil
 }
 
 //  Set query hints. `hints` is a JSON string
@@ -138,64 +138,64 @@ func (q *EjQuery) AddOr(orquery string) (*EjdbError) {
 //                      }
 //                    }
 func (q *EjQuery) SetHints(hints string) *EjdbError {
-	bsdata := bson_from_json(hints).data
-	ret := C.ejdbqueryhints(q.ejdb.ptr, q.ptr, unsafe.Pointer(bsdata))
-	if ret == nil {
-		return q.ejdb.check_error()
-	}
-	return nil
+    bsdata := bson_from_json(hints).data
+    ret := C.ejdbqueryhints((*C.struct_EJDB)(unsafe.Pointer(q.ejdb.ptr)), (*C.struct_EJQ)(unsafe.Pointer(q.ptr)), unsafe.Pointer(bsdata))
+    if ret == nil {
+        return q.ejdb.check_error()
+    }
+    return nil
 }
 
 // Execute the query and return all results as a slice of BSON objects
 func (q *EjQuery) Execute(coll *EjColl) ([][]byte, *EjdbError) {
-	// execute query
-	var count C.uint32_t
-	res := C.ejdbqryexecute(coll.ptr, q.ptr, &count, 0, nil)
-	defer C.ejdbqresultdispose(res)
-	err := coll.ejdb.check_error()
+    // execute query
+    var count C.uint32_t
+    res := C.ejdbqryexecute((*C.struct_EJCOLL)(unsafe.Pointer(coll.ptr)), (*C.struct_EJQ)(unsafe.Pointer(q.ptr)), &count, 0, nil)
+    defer C.ejdbqresultdispose(res)
+    err := coll.ejdb.check_error()
 
-	// return results
-	ret := make([][]byte, 0)
-	for i := 0; i < int(count); i++ {
-		var size C.int
-		bson_blob := C.ejdbqresultbsondata(res, C.int(i), &size)
-		el := make([]byte, int(size))
-		copy(el, (*[maxslice]byte)(bson_blob)[:int(size)])
-		ret = append(ret, el)
-	}
+    // return results
+    ret := make([][]byte, 0)
+    for i := 0; i < int(count); i++ {
+        var size C.int
+        bson_blob := C.ejdbqresultbsondata(res, C.int(i), &size)
+        el := make([]byte, int(size))
+        copy(el, (*[maxslice]byte)(bson_blob)[:int(size)])
+        ret = append(ret, el)
+    }
 
-	return ret, err
+    return ret, err
 }
 
 // Execute the query and return only the first result as a BSON object
 func (q *EjQuery) ExecuteOne(coll *EjColl) (*[]byte, *EjdbError) {
-	// execute query
-	var count C.uint32_t
-	res := C.ejdbqryexecute(coll.ptr, q.ptr, &count, jbqryfindone, nil)
-	defer C.ejdbqresultdispose(res)
-	err := coll.ejdb.check_error()
+    // execute query
+    var count C.uint32_t
+    res := C.ejdbqryexecute((*C.struct_EJCOLL)(unsafe.Pointer(coll.ptr)), (*C.struct_EJQ)(unsafe.Pointer(q.ptr)), &count, jbqryfindone, nil)
+    defer C.ejdbqresultdispose(res)
+    err := coll.ejdb.check_error()
 
-	// return results
-	if count == 0 {
-		return nil, err
-	}
+    // return results
+    if count == 0 {
+        return nil, err
+    }
 
-	var size C.int
-	bson_blob := C.ejdbqresultbsondata(res, 0, &size)
-	ret := make([]byte, int(size))
-	copy(ret, ((*[maxslice]byte)(bson_blob))[:int(size)])
-	return &ret, err
+    var size C.int
+    bson_blob := C.ejdbqresultbsondata(res, 0, &size)
+    ret := make([]byte, int(size))
+    copy(ret, ((*[maxslice]byte)(bson_blob))[:int(size)])
+    return &ret, err
 }
 
 // Execute the query and only return the number of results it returned, not the results themselves
 func (q *EjQuery) Count(coll *EjColl) (int, *EjdbError) {
-	var count C.uint32_t
-	C.ejdbqryexecute(coll.ptr, q.ptr, &count, jbqrycount, nil)
-	err := coll.ejdb.check_error()
-	return int(count), err
+    var count C.uint32_t
+    C.ejdbqryexecute((*C.struct_EJCOLL)(unsafe.Pointer(coll.ptr)), (*C.struct_EJQ)(unsafe.Pointer(q.ptr)), &count, jbqrycount, nil)
+    err := coll.ejdb.check_error()
+    return int(count), err
 }
 
 // Delete the query. This must be called in order to not leak memory.
 func (q *EjQuery) Del() {
-	C.ejdbquerydel(q.ptr)
+    C.ejdbquerydel((*C.struct_EJQ)(unsafe.Pointer(q.ptr)))
 }
